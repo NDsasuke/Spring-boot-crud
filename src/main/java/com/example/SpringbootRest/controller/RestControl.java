6c9429d7ba3a04cac2ad60592422d3e0a2854f84
NDsasuke~ <nishaldranasinghe@gmail.com~>
2023-03-10 12:33:06 +0530

    add endpoints

--- src/main/java/com/example/SpringbootRest/controller/RestControl.java
+++ src/main/java/com/example/SpringbootRest/controller/RestControl.java
@@ -1,4 +1,28 @@
 package com.example.SpringbootRest.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class RestControl {
+
+    @PostMapping("/add")
+    public void addRecord(){
+
+    }
+
+    @GetMapping("/all")
+    public void findAllRecord(){
+
+    }
+
+    @GetMapping("/find/{id}")
+    public void findById(){
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void removeById(){
+
+    }
 }
